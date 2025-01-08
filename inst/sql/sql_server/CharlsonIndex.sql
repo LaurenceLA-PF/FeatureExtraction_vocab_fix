40afea7e6518cb0f7205d5011338ce4575e0443f
Lawrence-Archer <LAWREL16@pfizer.com>
2025-01-08 14:33:58 +0000

    Attempt1

--- inst/sql/sql_server/CharlsonIndex.sql
+++ inst/sql/sql_server/CharlsonIndex.sql
@@ -33,7 +33,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 1,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4329847);
 
 --Congestive heart failure
@@ -54,7 +54,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 2,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (316139);
 
 --Peripheral vascular disease
@@ -75,7 +75,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 3,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (321052);
 
 --Cerebrovascular disease
@@ -96,7 +96,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 4,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (381591, 434056);
 
 --Dementia
@@ -117,7 +117,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 5,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4182210);
 
 --Chronic pulmonary disease
@@ -138,7 +138,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 6,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4063381);
 
 --Rheumatologic disease
@@ -159,7 +159,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 7,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (257628, 134442, 80800, 80809, 256197, 255348);
 
 --Peptic ulcer disease
@@ -180,7 +180,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 8,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4247120);
 
 --Mild liver disease
@@ -201,7 +201,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 9,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4064161, 4212540);
 
 --Diabetes (mild to moderate)
@@ -222,7 +222,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 10,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (201820);
 
 --Diabetes with chronic complications
@@ -243,7 +243,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 11,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (443767, 442793);
 
 --Hemoplegia or paralegia
@@ -264,7 +264,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 12,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (192606, 374022);
 
 --Renal disease
@@ -285,7 +285,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 13,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4030518);
 
 --Any malignancy
@@ -306,7 +306,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 14,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (443392);
 
 --Moderate to severe liver disease
@@ -327,7 +327,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 15,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (4245975, 4029488, 192680, 24966);
 
 --Metastatic solid tumor
@@ -348,7 +348,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 16,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (432851);
 
 --AIDS
@@ -369,7 +369,7 @@ INSERT INTO #charlson_concepts (
 	)
 SELECT 17,
 	descendant_concept_id
-FROM @cdm_database_schema.concept_ancestor
+FROM @vocabulary_database_schema.concept_ancestor
 WHERE ancestor_concept_id IN (439727);
 
 -- Feature construction
